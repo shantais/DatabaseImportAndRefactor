98e5c42bc31f270149fb453195a88d4486e8b5b7
shantais <shantais90@gmail.com>
2024-12-03 08:52:58 +0100

    added input for pages if they don't exist

--- doi_cutter.py
+++ doi_cutter.py
@@ -4,5 +4,5 @@ def cut(doi, doi_j, doi_i):
 
     # print("doi_i: " + doi_i + "\ndoi:   " + doi)
     if (doi_i == '-') or (doi_i not in doi and doi != '-'):
-        doi_i = doi[:31]
+        doi_i = doi[:37]
     return doi_j, doi_i
